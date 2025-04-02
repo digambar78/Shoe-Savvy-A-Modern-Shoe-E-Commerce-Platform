fdea87c01604db0fe1140eb450045bbe5c3e75eb
Digambar Singh <digambarsingh@Digambars-MacBook-Air.local>
2025-04-02 18:05:08 +0530

    Initial commit

---
+++ src/components/Home/Team.js
@@ -0,0 +1,6 @@
+const Team = [
+    {id:"team1",imageSource:"/images/team-img/digambar.png",name:"Digambar Singh",job:"Frontend"},
+    // {id:"team2",imageSource:"/images/team-img/love.png",name:"Love Kumar Agrawal",job:"Backend"},
+]
+
+export default Team;
